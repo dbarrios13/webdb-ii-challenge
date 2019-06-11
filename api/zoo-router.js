51db14f8dfed88b09c52212373f0902366768cfc
dbarrios13 <barriosadavid@gmail.com>
2019-06-11 13:58:21 -0700

    add delete, and put requests. working on testing the crud requests

--- api/zoo-router.js
+++ api/zoo-router.js
@@ -4,41 +4,86 @@ const router = require('express').Router()
 
 router.get('/', async (req, res) => {
     try {
-        
+        const zoos = await db.find()
+        res.status(200).json(zoos)
     } catch (error) {
-        
+        res.status(500).json({
+            message: 'Error retrieving zoos'
+        })
     }
 })
 
 router.get('/:id', async (req, res) => {
     try {
-        
+        const zoo = await db.findById(req.params.id)
+        if(zoo) {
+            res.status(200).json(zoo)
+        } else {
+            res.status(404).json({
+                message: 'The zoo with the specified ID does not exist'
+            })
+        }
     } catch (error) {
-        
+        res.status(500).json({
+            message: 'Error retrieving zoo'
+        })
     }
 })
 
 router.post('/', async (req, res) => {
     try {
-        
+        const zoo = await db.add(req.body)
+        if(req.body.name) {
+            res.status(201).json(zoo)
+        } else {
+            res.status(400).json({
+                message: 'Please provide name of the zoo'
+            })
+        }
     } catch (error) {
-        
+        res.status(500).json({
+            message: 'Error adding zoo'
+        })
     }
 })
 
 router.put('/:id', async (req, res) => {
     try {
-        
+        const zoo = await db.update(req.params.id, req.body)
+        if (zoo) {
+            if (req.body.name) {
+                res.status(200).json(zoo)
+            } else {
+                res.status(400).json({
+                    message: 'Please provide name of the zoo'
+                })
+            }
+        } else {
+            res.status(404).json({
+                message: 'The zoo with the specified ID does not exist'
+            })
+        }
     } catch (error) {
-        
+        res.status(500).json({
+            message: 'Error updating zoo'
+        })
     }
 })
 
 router.delete('/:id', async (req, res) => {
     try {
-        
+        const zoo = await db.remove(req.params.id)
+        if(zoo) {
+            res.status(204).end()
+        } else {
+            res.status(404).json({
+                message: 'The zoo with the specified ID does not exist'
+            })
+        }
     } catch (error) {
-        
+        res.status(500).json({
+            message: 'Error deleting zoo'
+        })
     }
 })
 
